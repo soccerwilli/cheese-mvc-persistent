c7dcf8d9fb8eb07969e6bb8ebc4e2f3ecf12f533
Matt Williams <soccerwilli@hotmail.com>
2017-07-07 14:35:45 -0600

    finished part I: view all categories

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package org.launchcode.controllers;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,4 +16,12 @@ public class CategoryController {
 
     @Autowired
     private CategoryDao categoryDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("title", "Categories");
+        model.addAttribute("categories", categoryDao.findAll());
+
+        return "categories/index";
+    }
 }
